6bba8575991cd36f5a2223ac4d3d56f85f667613
Gonzalo Torreras <GonzaloTorreras@users.noreply.github.com>
2020-09-20 22:32:24 +0200

    Nikola auto commit.
    Nikola version: 8.1.1

--- src/conf.py
+++ src/conf.py
@@ -77,6 +77,7 @@ NAVIGATION_LINKS = {
         ("/archive.html", "Archive"),
         ("/categories/", "Tags"),
         ("/rss.xml", "RSS feed"),
+
         (
             (
                 ("/pages","Pages 1.1"),
@@ -551,11 +552,11 @@ REDIRECTIONS = []
 # For more details, read the manual:
 # https://getnikola.com/handbook.html#deploying-to-github
 # You will need to configure the deployment branch on GitHub.
-GITHUB_SOURCE_BRANCH = 'https://github.com/GonzaloTorreras/nikola-theme-skelleton/'
+GITHUB_SOURCE_BRANCH = 'master'
 GITHUB_DEPLOY_BRANCH = 'gh-pages'
 
 # The name of the remote where you wish to push to, using github_deploy.
-GITHUB_REMOTE_NAME = 'origin'
+GITHUB_REMOTE_NAME = 'https://github.com/GonzaloTorreras/nikola-theme-skelleton/'
 
 # Whether or not github_deploy should commit to the source branch automatically
 # before deploying.
